9a7be9d0d3714988c6637326d38a3ee7121f79d5
khangpv202 <khangpv202@gmail.com>
2013-06-10 13:38:17 +0700

    create initial functions

---
+++ src/main/java/BankAccountDAO/BankAccountDAO.java
@@ -0,0 +1,11 @@
+package BankAccountDAO;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: khangpv
+ * Date: 6/10/13
+ * Time: 1:37 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class BankAccountDAO {
+}
---
+++ src/test/java/TestAccount.java
@@ -0,0 +1,14 @@
+import BankAccountDAO.BankAccountDAO;
+
+import static org.mockito.Mockito.mock;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: khangpv
+ * Date: 6/10/13
+ * Time: 1:35 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class TestAccount {
+    BankAccountDAO mockAccount = mock(BankAccountDAO.class);
+}
